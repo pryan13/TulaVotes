23406c07e3860fc936a0678e18f18d47dad31023
vitolld <vitolld@gmail.com>
2014-05-28 18:03:33 +0300

    implement e2e tests for login page

--- test/e2e/loginScenario.js
+++ test/e2e/loginScenario.js
@@ -1,6 +1,28 @@
-describe('homepage >', function(){
-	it('should redirect to login page if not authorized', function(){
-		browser.driver.get('http://localhost:3000/');
-		expect(browser.driver.getTitle().toEqual('Enter to vote please'));
+describe('Login page >', function(){
+	var index = {
+		url: ['http://localhost:3000/', 'http://localhost:3000/#/index'],
+		title: 'Tula Votes!'
+	};
+	var login = {
+		url: ['http://localhost:3000/login'],
+		title: 'Enter to vote please'
+	}
+
+	browser.ignoreSynchronization = true;
+
+	it('should be represented instead of index if not authorized', function(){
+		for(var i = 0; i < index.url.length; i++){
+			browser.driver.get(index.url[i]);
+			expect(browser.driver.getCurrentUrl()).toMatch(login.url[0]);
+			expect(browser.driver.getTitle()).toEqual(login.title);
+		}
+	});
+
+	it('should redirect user to the index page after submit email', function(){
+		browser.driver.get(index.url[0]);
+		element(by.name('email')).sendKeys('test@mail.com');
+		element(by.buttonText('OK')).click();
+		expect(browser.driver.getCurrentUrl()).toEqual(index.url[1]);
+		expect(browser.driver.getTitle()).toEqual(index.title);
 	});
 });
--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,19 +1,24 @@
 exports.config = {
-	//baseUrl: 'http://localhost:3000/',
-
-	seleniumAddress: 'http://localhost:3000/',
-
+	// Do not start a Selenium Standalone sever - only run this using chrome.
 	chromeOnly: true,
+	chromeDriver: '../node_modules/protractor/selenium/chromedriver',
 
+	// Capabilities to be passed to the webdriver instance.
 	capabilities: {
-		'browserName': 'chrome'
+		'browserName': 'chrome',
+		'chromeOptions': {
+			'args': ['test-type=true']
+		}
 	},
 
+	// Spec patterns are relative to the current working directly when
+	// protractor is called.
 	specs: ['e2e/*.js'],
 
-	framework: 'jasmine',
-
+	// Options to be passed to Jasmine-node.
 	jasmineNodeOpts: {
 		showColors: true,
+		isVerbose: true,
+		defaultTimeoutInterval: 30000
 	}
 };
